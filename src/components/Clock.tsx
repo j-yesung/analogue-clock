50fc583448a1ecc38f81957cf0a7380a7e81d561
j-yesung <hisung-ah@kakao.com>
2024-03-14 21:31:27 +0900

    시계 바늘 위치 변수 및 interval 로직 훅으로 분리

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
 import * as S from './Clock.styled';
 import ClockNumber from './ClockNumber';
+import { useClock } from '../hooks/useClock';
 
 const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const timerId = setInterval(() => setDate(new Date()), 1000);
-
-    return () => {
-      clearInterval(timerId);
-    };
-  }, []);
-
-  const secondsRatio = date.getSeconds() / 60;
-  const minutesRatio = (secondsRatio + date.getMinutes()) / 60;
-  const hoursRatio = (minutesRatio + date.getHours()) / 12;
-
-  const secondDegrees = secondsRatio * 360;
-  const minuteDegrees = minutesRatio * 360;
-  const hourDegrees = hoursRatio * 360;
+  const { hourDegrees, minuteDegrees, secondDegrees } = useClock(0);
 
   return (
     <>
--- src/hooks/useClock.ts
+++ src/hooks/useClock.ts
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+
 export const useClock = (angle: number) => {
+  const [date, setDate] = useState(new Date());
+
+  const secondsRatio = date.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + date.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + date.getHours()) / 12;
+
+  const secondDegrees = secondsRatio * 360;
+  const minuteDegrees = minutesRatio * 360;
+  const hourDegrees = hoursRatio * 360;
+
   const radius = 170;
   const radian = (angle * Math.PI) / 180;
   const x = radius * Math.sin(radian);
   const y = -radius * Math.cos(radian);
 
-  return { x, y };
+  useEffect(() => {
+    const timerId = setInterval(() => setDate(new Date()), 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return { x, y, secondDegrees, minuteDegrees, hourDegrees };
 };
